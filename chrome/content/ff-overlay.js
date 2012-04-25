ec61ced07ddd18c375d54f5021ec34ea12826559
senicar <senicar@gmail.com>
2012-04-25 17:20:30 +0200

    makePopupMenu, same code for displaying visibleTabsMenu and tabsGroupsMenu.

--- chrome/content/ff-overlay.js
+++ chrome/content/ff-overlay.js
@@ -68,10 +68,13 @@ senicar.emc = (function (emc)
 			found,
 			x, y;
 	  
-		for ( x = 0; x < origLen; x++ ) {
+		for ( x = 0; x < origLen; x++ )
+		{
 			found = undefined;  
-			for ( y = 0; y < newArr.length; y++ ) {
-				if ( origArr[x] === newArr[y] ) {
+			for ( y = 0; y < newArr.length; y++ )
+			{
+				if ( origArr[x] === newArr[y] )
+				{
 				  found = true;
 				  break;
 				}
@@ -127,6 +130,53 @@ senicar.emc = (function (emc)
 			historySidebarToggle();
 	}
 
+	var makePopupMenu = function(items, refresh)
+	{
+		items = ( typeof items == 'undefined' ) ? false : items;
+
+		var tabs_popup = document.createElement("menupopup");
+		tabs_popup.setAttribute("oncommand", "gBrowser.tabContainer.selectedIndex = event.target.getAttribute('index');");
+		//tabs_popup.setAttribute("onclick", "checkForMiddleClick(this, event);");
+		tabs_popup.setAttribute("onclick", "senicar.emc.closeTab(this, event);");
+		document.getElementById("mainPopupSet").appendChild(tabs_popup);
+
+		for ( var i = 0; i< items.length; i++)
+		{
+			var tab = items[i];
+
+			if(tab == 'separator')
+				var menuseparator = tabs_popup.appendChild(document.createElement("menuseparator"));
+			else
+			{
+				var item = tabs_popup.appendChild(document.createElement("menuitem"));
+
+				item.setAttribute("index", tab._tPos);
+				item.setAttribute("label", tab.label);
+				
+				// if page has no favicon show default
+				if(!tab.getAttribute("image"))
+					item.setAttribute("image", 'chrome://mozapps/skin/places/defaultFavicon.png');
+				else
+					item.setAttribute("image", tab.getAttribute("image"));
+
+				item.classList.add("menuitem-iconic");
+
+				if (tab.selected)
+					item.classList.add("unified-nav-current");
+			}
+		}
+
+		if(refresh)
+			tabs_popup.openPopupAtScreen(screenX, screenY, true);
+		else 
+		{
+			screenX = mouseEvent.screenX;
+			screenY = mouseEvent.screenY;
+			tabs_popup.openPopupAtScreen(mouseEvent.screenX, mouseEvent.screenY, true);
+		}
+
+	}
+
 
 	//////////////////
 	//
@@ -157,15 +207,13 @@ senicar.emc = (function (emc)
 		var tab = gBrowser.tabContainer.getItemAtIndex(item.target.getAttribute('index'));
 		if(item.button == 1)
 		{
-			menu.hidePopup();
 			gBrowser.removeTab(tab);
-
+			menu.hidePopup();
 			if( action == 'tabs' || action == 'visibleTabsMenu' )
 				visibleTabsMenu(true);
 
 			if( action  == 'tabsGroupsMenu')
 				tabsGroupsMenu(true);
-			// TODO: object.removeChild(event.target); and refresh menu
 		}
 	}
 
@@ -177,136 +225,57 @@ senicar.emc = (function (emc)
 	//////////////////
 
 
-	// Display currently visible tabs
 	var visibleTabsMenu= function (refresh)
 	{
 		refresh = ( typeof refresh == 'undefined' ) ? false : true;
-
-		var tabs_popup = document.createElement("menupopup");
-		tabs_popup.setAttribute("oncommand", "gBrowser.tabContainer.selectedIndex = event.target.getAttribute('index');");
-		//tabs_popup.setAttribute("onclick", "checkForMiddleClick(this, event);");
-		tabs_popup.setAttribute("onclick", "senicar.emc.closeTab(this, event);");
-		document.getElementById("mainPopupSet").appendChild(tabs_popup);
-
-		var num = gBrowser.visibleTabs.length;
-		for ( var i = 0; i< num; i++) {
-			var tab = gBrowser.visibleTabs[i];
-			var item = tabs_popup.appendChild(document.createElement("menuitem"));
-			item.setAttribute("index", tab._tPos);
-			item.setAttribute("label", tab.label);
-            
-            // if page has no favicon show default
-			if(!tab.getAttribute("image")) {
-				item.setAttribute("image", 'chrome://mozapps/skin/places/defaultFavicon.png');
-			}
-			else {
-				item.setAttribute("image", tab.getAttribute("image"));
-			}
-
-			item.classList.add("menuitem-iconic");
-			if (tab.selectedIndex == i) {
-				item.classList.add("unified-nav-current");
-			}
-			
-			var bundle_browser = document.getElementById("bundle_browser");
-		}
-
-		if(refresh)
-			tabs_popup.openPopupAtScreen(screenX, screenY, true);
-		else
-		{
-			screenX = mouseEvent.screenX;
-			screenY = mouseEvent.screenY;
-			tabs_popup.openPopupAtScreen(mouseEvent.screenX, mouseEvent.screenY, true);
-		}
+		makePopupMenu(gBrowser.visibleTabs, refresh);
 	}
 
 
 	var tabsGroupsMenu = function (refresh)
 	{
 		refresh = ( typeof refresh == 'undefined' ) ? false : true;
-
-		var tabs_popup = document.createElement("menupopup");
-		tabs_popup.setAttribute("oncommand", "gBrowser.tabContainer.selectedIndex = event.target.getAttribute('index');");
-		tabs_popup.setAttribute("onclick", "senicar.emc.closeTab(this, event);");
-		document.getElementById("mainPopupSet").appendChild(tabs_popup);
-
 		var num = gBrowser.browsers.length;
-
 		var tab_id;
 		var parent_id;
 		var groups = [];
-		for ( var x = 0; x< num; x++) {
+		var tabs = [];
+
+		for ( var x = 0; x< num; x++)
+		{
 			tab_id = gBrowser.tabContainer.getItemAtIndex(x);
-			if(tab_id.pinned) {
+			if(tab_id.pinned)
 				groups.push(0);
-			}
-			else {
-				parent_id = tab_id._tabViewTabItem.parent.id;
-				groups.push(parent_id);
-			}
+			else
+				groups.push(tab_id._tabViewTabItem.parent.id);
 		}
 
 		var uniq = uniqueArr(groups);
 
-		for (var x = 0; x < uniq.length; x++) {
+		for (var x = 0; x < uniq.length; x++)
+		{
 			parent_id=uniq[x];
 			uniq[x] = [];
+
+			if(x != 0)
+				tabs.push('separator');
+
 			for ( var y = 0; y < num; y++) {
 				tab_id = gBrowser.tabContainer.getItemAtIndex(y);
-				if(tab_id.pinned) {
+				if(tab_id.pinned)
+				{
 					if(parent_id == 0)
-						uniq[x].push(tab_id);
+						tabs.push(tab_id);
 				}
-				else {
-					if(tab_id._tabViewTabItem.parent.id == parent_id) {
-						uniq[x].push(tab_id);
-					}
+				else
+				{
+					if(tab_id._tabViewTabItem.parent.id == parent_id)
+						tabs.push(tab_id);
 				}
 			}
 		}
 
-		for ( var i = 0; i< uniq.length; i++)
-		{
-			var numz = uniq[i].length;
-			if(i>0)
-			{
-				var menuseparator = tabs_popup.appendChild(document.createElement("menuseparator"));
-			}
-
-			for ( var z = 0; z < numz; z++)
-			{
-				var tab = uniq[i][z];
-
-				var item = tabs_popup.appendChild(document.createElement("menuitem"));
-				item.setAttribute("index", tab._tPos);
-				item.setAttribute("label", tab.label);
-				
-				// if page has no favicon show default
-				if(!tab.getAttribute("image")) {
-					item.setAttribute("image", 'chrome://mozapps/skin/places/defaultFavicon.png');
-				}
-				else {
-					item.setAttribute("image", tab.getAttribute("image"));
-				}
-
-				item.classList.add("menuitem-iconic");
-				if (tab.selectedIndex == i) {
-					item.classList.add("unified-nav-current");
-				}
-				
-				var bundle_browser = document.getElementById("bundle_browser");
-			}
-		}
-
-		if(refresh)
-			tabs_popup.openPopupAtScreen(screenX, screenY, true);
-		else
-		{
-			screenX = mouseEvent.screenX;
-			screenY = mouseEvent.screenY;
-			tabs_popup.openPopupAtScreen(mouseEvent.screenX, mouseEvent.screenY, true);
-		}
+		makePopupMenu(tabs, refresh);
 	}
 
 
@@ -320,7 +289,8 @@ senicar.emc = (function (emc)
 		var hasHistory = FillHistoryMenu(history_popup);
 		var selectedTab = gBrowser.tabContainer.selectedItem;
 		
-		if(!hasHistory) {
+		if(!hasHistory)
+		{
 
 			var menuitem = history_popup.appendChild(document.createElement("menuitem"));
 			menuitem.setAttribute("index", "0");
