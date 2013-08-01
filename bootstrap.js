521a8f1c320927a5d1000b4df1aa16acc717f2df
senicar <senicar@gmail.com>
2013-08-01 13:01:07 +0200

    click detection, area validation, defaultPref now in bootstrap.js

--- bootstrap.js
+++ bootstrap.js
@@ -6,26 +6,239 @@
 var {classes: Cc, interfaces: Ci, utils: Cu} = Components;
 Cu.import("resource://gre/modules/Services.jsm");
 
+
+
+
+
 // ********************************************************************************************* //
 // Constants
 
+
+const DBG_EMC = true;
+const BRANCH = Services.prefs.getBranch("extensions.enhancedmiddleclick.");
+const DEFAULT_PREFS = {
+	mainMenu: "history",
+	secondaryMenu: "history",
+	refreshOnTabClose: true,
+	displayGroupName: true,
+	autoscrolling: false
+};
+
+
 // Default preferences for bootstrap extensions are registered dynamically.
-var defaultPrefs =
-{
-	"DBG_EMC": true,
-	"BRANCH": Services.prefs.getBranch("extensions.enhancedmiddleclick."),
+// no need for default/preferences/prefs.js
+function setDefaultPrefs() {
+	// http://starkravingfinkle.org/blog/2011/01/restartless-add-ons-%E2%80%93-default-preferences/
+
+	for(let [key, val] in Iterator(DEFAULT_PREFS)) {
+		switch (typeof val) {
+			case "boolean":
+				BRANCH.setBoolPref(key, val);
+				break;
+			case "number":
+				BRANCH.setIntPref(key, val);
+				break;
+			case "string":
+				BRANCH.setCharPref(key, val);
+				break;
+		}
+	}
 }
+
+
+
+
+
+// ********************************************************************************************* //
+// Main EMC functions
+
+
+// used for debuging
+var emclogger = function(msg)
+{
+	// check if firebug even available, if you try to "report" and firebug
+	// is not opened it will break addon
+	if(DBG_EMC) {
+		if(typeof Firebug == 'object')
+			Firebug.Console.log(msg);
+
+		Services.console.logStringMessage("enhancedmiddleclick: " + msg);
+	}
+}
+
+
+// this function is called by addEventListener click
+var clicker = function(e) {
+	emclogger("clicker");
+	let window = this.window;
+
+	if( areaValidator(e, window) ) {
+		// e.cancelBubble = true;
+		e.stopPropagation();
+		emclogger("click passed");
+		//display();
+	}
+};
+
+
+// it validates only on empty page areas, at least it tries
+var areaValidator = function(e, window)
+{
+	let t = e.target;
+	emclogger(t);
+
+	// by default disable all target areas
+	let disallow = {};
+	let allow = {};
+
+	disallow.html = false;
+	disallow.xul = false;
+	allow.html = false;
+	allow.xul = false;
+
+	// bootstrap is not loaded into a window thats why HTMLElements
+	// cannot be directly accessed, again we have to use window
+	// https://developer.mozilla.org/en-US/docs/Gecko_DOM_Reference
+	if( t instanceof window.HTMLInputElement ||
+		t instanceof window.HTMLAnchorElement ||
+		t instanceof window.HTMLButtonElement ||
+		t instanceof window.HTMLVideoElement ||
+		t instanceof window.HTMLAudioElement ||
+		t instanceof window.HTMLTextAreaElement ||
+		t instanceof window.HTMLCanvasElement ||
+		t instanceof window.HTMLAppletElement ||
+		t instanceof window.HTMLSelectElement ||
+		t instanceof window.HTMLOptionElement ||
+		t.attributes["g_editable"]
+	) { disallow.html = true; }
+
+	// best way to disable all xul elements is by instanceof XULElement
+	if( t instanceof window.XULControllers ||
+		t.nodeName == 'textbox' ||
+		t.nodeName == 'toolbarbutton' ||
+		t.nodeName == 'richlistitem' ||
+		t.nodeName == 'menuitem'
+	) { disallow.xul = true; }
+
+	if( t.baseURI == 'about:addons' ||
+		t.localName == 'tabbrowser'
+	) { allow.xul = true ; }
+
+	if( t instanceof window.HTMLElement ||
+		t instanceof window.SVGElement
+	) { allow.html = true; }
+
+	// check if element is child of anchor
+	// we realy dont want to break links
+	while(t && !(t instanceof window.HTMLAnchorElement)) {
+		t = t.parentNode;
+		if(t instanceof window.HTMLAnchorElement)
+			disallow.html = true;
+	}
+
+	if( (allow.html || allow.xul) && !(disallow.html || disallow.xul) )
+		return true;
+	else
+		return false;
+};
+
+
+var loadIntoWindow = function(window) {
+	if (!window)
+		return;
+	// Add any persistent UI elements
+	// Perform any other initialization
+	emclogger("add click listener");
+
+	// true, to execute before selection buffer on linux
+	window.addEventListener("click", clicker, true);
+}
+
+
+var unloadFromWindow = function(window) {
+	if (!window)
+		return;
+	// Remove any persistent UI elements
+	// Perform any other cleanup
+	emclogger("remove click listener");
+	window.removeEventListener("click", clicker, true);
+}
+
+
+var windowListener = {
+	onOpenWindow: function(aWindow) {
+		// Wait for the window to finish loading
+		let domWindow = aWindow.QueryInterface(Ci.nsIInterfaceRequestor).getInterface(Ci.nsIDOMWindowInternal || Ci.nsIDOMWindow);
+		domWindow.addEventListener("load", function() {
+			domWindow.removeEventListener("load", arguments.callee, false);
+			loadIntoWindow(domWindow);
+		}, false);
+	},
+
+	onCloseWindow: function(aWindow) {},
+	onWindowTitleChange: function(aWindow, aTitle) {}
+};
+
+
+
+
+
+// ********************************************************************************************* //
+// Actions
+
+
+
+
 
 // ********************************************************************************************* //
 // Firefox Bootstrap API
-function install(data, reason) {}
 
-function uninstall(data, reason) {
-	// delete all preferences on this branch
-	defaultPrefs.BRANCH.deleteBranch("");
+
+function install(data, reason) {
+	emclogger("install");
+	setDefaultPrefs();
 }
 
-function startup(data, reason) {}
 
-function shutdown(data, reason) {}
-// ********************************************************************************************* //
+function uninstall(data, reason) {
+	emclogger("uninstall");
+	// delete all preferences on this branch
+	BRANCH.deleteBranch("");
+}
+
+
+function startup(data, reason) {
+	emclogger("startup");
+	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+	// Load into any existing windows
+	// bootstrap.js is not loaded into a window so we have to do it manually
+	let windows = wm.getEnumerator("navigator:browser");
+	while (windows.hasMoreElements()) {
+		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+		loadIntoWindow(domWindow);
+	}
+	// Load into any new windows
+	wm.addListener(windowListener);
+}
+
+
+function shutdown(data, reason) {
+	emclogger("shutdown");
+	// When the application is shutting down we normally don't have to clean
+	// up any UI changes made
+	if (reason == APP_SHUTDOWN)
+		return;
+
+	let wm = Cc["@mozilla.org/appshell/window-mediator;1"].getService(Ci.nsIWindowMediator);
+
+	// Stop listening for new windows
+	wm.removeListener(windowListener);
+
+	// Unload from any existing windows
+	let windows = wm.getEnumerator("navigator:browser");
+	while (windows.hasMoreElements()) {
+		let domWindow = windows.getNext().QueryInterface(Ci.nsIDOMWindow);
+		unloadFromWindow(domWindow);
+	}
+}
--- defaults/preferences/prefs.js
+++ defaults/preferences/prefs.js
@@ -1,8 +1,8 @@
-pref("extensions.enhancedmiddleclick.refreshOnTabClose", false);
-pref("extensions.enhancedmiddleclick.autoscrolling", false);
-pref("extensions.enhancedmiddleclick.mainMenu", "history");
-pref("extensions.enhancedmiddleclick.secondaryMenu", "disable");
-pref("extensions.enhancedmiddleclick.displayGroupName", false);
+// pref("extensions.enhancedmiddleclick.refreshOnTabClose", false);
+// pref("extensions.enhancedmiddleclick.autoscrolling", false);
+// pref("extensions.enhancedmiddleclick.mainMenu", "history");
+// pref("extensions.enhancedmiddleclick.secondaryMenu", "disable");
+// pref("extensions.enhancedmiddleclick.displayGroupName", false);
 
 // https://developer.mozilla.org/en/Localizing_extension_descriptions
 pref("extensions.enhancedmiddleclick@senicar.net.description", "chrome://enhancedmiddleclick/locale/ff-overlay.properties")
